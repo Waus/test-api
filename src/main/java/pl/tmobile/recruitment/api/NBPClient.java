3c1b5dbb69e10d910334ca22284d76f4e0a5672c
Kamil Murański <kamil.muranski.official@gmail.com>
2025-03-05 11:51:56 +0100

    Poprawka NBPClient żeby korzystała z REST Assured

--- src/main/java/pl/tmobile/recruitment/api/NBPClient.java
+++ src/main/java/pl/tmobile/recruitment/api/NBPClient.java
@@ -1,38 +1,36 @@
 package pl.tmobile.recruitment.api;
 
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import pl.tmobile.recruitment.model.ExchangeRateTable;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.Scanner;
 
 public class NBPClient {
     private static final Logger logger = LogManager.getLogger(NBPClient.class);
     private static final String API_URL = "http://api.nbp.pl/api/exchangerates/tables/A/?format=json";
 
-    public ExchangeRateTable fetchExchangeRates() throws IOException {
-        URL url = new URL(API_URL);
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestMethod("GET");
+    public ExchangeRateTable fetchExchangeRates() {
+        logger.info("Wysyłam zapytanie GET do NBP API: {}", API_URL);
 
-        int responseCode = conn.getResponseCode();
-        logger.info("Kod odpowiedzi: {}", responseCode);
+        Response response = RestAssured
+                .given()
+                .when()
+                .get(API_URL)
+                .then()
+                .statusCode(200)
+                .extract()
+                .response();
 
-        if (responseCode != 200) {
-            logger.error("Nie udało się pobrać danych NBP API! Status: {}", responseCode);
-            throw new RuntimeException("Nie udało się pobrać danych NBP API");
+        logger.info("Kod odpowiedzi: {}", response.statusCode());
+
+        ExchangeRateTable[] tables = response.as(ExchangeRateTable[].class);
+
+        if (tables.length == 0) {
+            logger.error("Brak danych w odpowiedzi z NBP API");
+            throw new RuntimeException("Brak danych w odpowiedzi z NBP API");
         }
 
-        Scanner scanner = new Scanner(url.openStream());
-        String response = scanner.useDelimiter("\\A").next();
-        scanner.close();
-
-        ObjectMapper mapper = new ObjectMapper();
-        ExchangeRateTable[] tables = mapper.readValue(response, ExchangeRateTable[].class);
-
-        return tables.length > 0 ? tables[0] : null;
+        return tables[0];
     }
 }
--- src/test/resources/features/test1.feature
+++ src/test/resources/features/test1.feature
@@ -1,8 +1,13 @@
 Feature: Exchange Rates from NBP
 
-  Scenario: Kursy walut
-    When  Pobieram kursy walut
-    Then Wyświetlam kurs dla waluty o kodzie "USD"
-    Then Wyświetlam kurs dla waluty o nazwie "dolar amerykański"
+ Scenario Outline: Kursy walut
+    When Pobieram kursy walut
+    Then Wyświetlam kurs dla waluty o kodzie <CurrencyId>
+    Then Wyświetlam kurs dla waluty o nazwie <Currency>
     Then Wyświetlam waluty o kursie powyżej 5
     Then Wyświetlam waluty o kursie poniżej 3
+   Examples:
+     | CurrencyId | Currency |
+     | 'USD'    |      "dolar amerykański"      |
+     | 'EUR'    | "euro"                        |
+
